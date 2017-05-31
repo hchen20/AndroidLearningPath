d05dd88d3639719fcc193997f626be0fb59c74e1
Hang Chen <hchen1991@gmail.com>
2017-05-31 01:29:11 -0400

    Adding FeedEntry and ParseApplications classes for Top10Downloader

---
+++ app/src/main/java/com/behappy/hchen/androidlearningpath/wechatsdk/WechatSDKLearn.java
@@ -0,0 +1,14 @@
+package com.behappy.hchen.androidlearningpath.wechatsdk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.behappy.hchen.androidlearningpath.R;
+public class WechatSDKLearn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wechat_sdklearn);
+    }
+}
